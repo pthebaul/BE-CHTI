f3f7d6579935dee374f10f05fdede42eea013fa4
Paul Thebault <pthebaul@etud.insa-toulouse.fr>
2020-03-30 11:02:11 +0200

    Ajout de la procédure M2dft

--- dft_virgule_fixe/dft.s
+++ dft_virgule_fixe/dft.s
@@ -1,1 +1,25 @@
 	AREA Dft, CODE, READONLY
+	IMPORT TabSin
+	IMPORT TabCos
+	IMPORT TabSig
+
+demiDFT	PROC
+	ENDP
+
+M2dft	PROC
+	PUSH	{r4, lr}
+	PUSH    {r0, r1}
+	LDR	r2, =TabCos
+	BL	demiDFT
+	MUL	r4, r0, r0
+
+	POP	{r0, r1}
+	LDR	r2, =TabSin
+	BL	demiDFT
+	MLA	r0, r0, r0, r4
+
+	LSR	r0, #15
+	POP    {r4, pc}
+	ENDP
+
+	END
