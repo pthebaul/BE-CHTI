771c17ab902f4326abced48430e3baf95f56fa50
Paul Thebault <pthebaul@etud.insa-toulouse.fr>
2020-03-31 18:25:43 +0200

    DFT fonctionnelle Des tests plus exhaustifs seront effectués à la prochaine séance

--- dft_virgule_fixe/principal.c
+++ dft_virgule_fixe/principal.c
@@ -1,7 +1,14 @@
 
+extern int M2dft(short*, int);
+extern short TabSig[];
+
+int tab[64];
+
 int main(void)
 {
-while	(1)
+	for (int k=0; k < 64; ++k)
 	{
+		tab[k] = M2dft(TabSig, k);
 	}
+	while (1) {}
 }
